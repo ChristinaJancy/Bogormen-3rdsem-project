46dbb597788d1163944c334d27d889bec8e9a357
ChristinaJancy <chri91eq@easv365.dk>
2020-12-03 18:51:03 +0100

    Update page.php

--- page.php
+++ page.php
@@ -18,13 +18,14 @@ get_header(); // This fxn gets the header.php file and renders it ?>
 
 					<article class="post">
 					
-						<h1 class="title"><?php the_title(); // Display the title of the page ?></h1>
+						<h2 class="title"><?php the_title(); // Display the title of the page ?></h2>
 						
 						<div class="the-content">
 							<?php the_content(); 
 							// This call the main content of the page, the stuff in the main text box while composing.
 							// This will wrap everything in p tags
 							?>
+							
 							<?php wp_link_pages(); // This will display pagination links, if applicable to the page ?>
 						</div><!-- the-content -->
 						
@@ -34,12 +35,12 @@ get_header(); // This fxn gets the header.php file and renders it ?>
 
 			<?php else : // Well, if there are no posts to display and loop through, let's apologize to the reader (also your 404 error) ?>
 				
-				<article class="post error">
-					<h1 class="404">Nothing posted yet</h1>
+				<article class="post error" style="text-align:center;">
+					<h1 class="404">Ups - denne side eksisterer vist ikke endnu.</h1>
 				</article>
 
 			<?php endif; // OK, I think that takes care of both scenarios (having a page or not having a page to show) ?>
 
 		</div><!-- #content .site-content -->
-	</div><!-- #primary .content-area -->
+    </div><!-- #primary .content-area -->
 <?php get_footer(); // This fxn gets the footer.php file and renders it ?>
